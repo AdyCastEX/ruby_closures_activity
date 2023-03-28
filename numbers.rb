85298ebe53c93dde68a85effdc0da7c4ffb3e18b
Joseph <josephvillanueva@github.com>
2023-03-28 16:55:15 +0800

    changed

--- numbers.rb
+++ numbers.rb
@@ -1,18 +1,15 @@
-class Numbers 
     def with_these_numbers(num1, num2) 
         yield(num1, num2)
     end
-       
-end
 
-Numbers.new.with_these_numbers(1, 4) do |first, second|
+with_these_numbers(1, 4) do |first, second|
     puts first + second
 end
 
-Numbers.new.with_these_numbers(3, 6) do |first, second|
+with_these_numbers(3, 6) do |first, second|
     puts first * second
 end
 
-Numbers.new.with_these_numbers(12, 3) do |first, second|
+with_these_numbers(12, 3) do |first, second|
     puts first / second
 end
