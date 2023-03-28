e6efcb22a9802e55f0dbff95d0fbe1e06eadc13f
KennedydeVera <deverakennedy@gmail.com>
2023-03-28 17:07:31 +0800

    re submit car

--- car.rb
+++ car.rb
@@ -1,9 +1,13 @@
 class Car
-  
+  attr_accessor :color, :plate_number, :manufacturer
+
+  def initialize()
+    yield self
+  end
 end
 
-car = Car.new do |c|
-  c.color = "Red"
-  c.plate_number = "ABC 1234"
-  c.manufacturer = "Toyota"
-end
+# car = Car.new do |c|
+#   c.color = "Red"
+#   c.plate_number = "ABC 1234"
+#   c.manufacturer = "Toyota"
+# end
